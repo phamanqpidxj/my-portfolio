e301df741bbc99bfabded42cd70a63cbda913fe7
Phạm An(GodEX) <152767312+phamanqpidxj@users.noreply.github.com>
2026-02-02 08:09:35 +0000

    Đổi tên từ Nguyễn Văn A sang Phạm Dĩ An

--- my-portfolio/my-portfolio/js/script.js
+++ my-portfolio/my-portfolio/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Nguyễn Văn A đã load xong!');
+    console.log('Portfolio của Phạm Dĩ An đã load xong!');
 
     alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
 });
