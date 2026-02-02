c59014e82a8537365f31b84c167eb5a2b5ff2400
Phạm An(GodEX) <152767312+phamanqpidxj@users.noreply.github.com>
2026-02-02 07:59:41 +0000

    Add portfolio website files

---
+++ my-portfolio/my-portfolio/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Nguyễn Văn A đã load xong!');
+    // Thông báo chào mừng người dùng khi truy cập trang
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
